b0a1e0510a46010de911fc2fe6019e90673c013b
toran billups <toranb@gmail.com>
2019-10-23 20:10:27 -0500

    init phx.new

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :game, GameWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "pVMvop5wr/HzuCdOAF1X4cNyXBWUl1lSUHFu7rmEuSHOlQnlr5gQWx7hpwz07yiI",
+  render_errors: [view: GameWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Game.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
