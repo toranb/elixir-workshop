7edbae497dd511536b0bb7090a249e5ea418573a
toran billups <toranb@gmail.com>
2019-10-23 20:27:15 -0500

    dump webpack setup for ease of workshop

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-import css from "../css/app.css"
-
-import "phoenix_html"
-import {Socket} from "phoenix"
-import LiveSocket from "phoenix_live_view"
-
-let liveSocket = new LiveSocket("/live", Socket)
-liveSocket.connect()
